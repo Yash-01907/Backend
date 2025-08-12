5835b9866e3f861909b50ee1cf5ddf93a8ffbea6
Yash Pandey <yashpandey1907@gmail.com>
2025-08-12 06:18:58 +0530

    Added user router and tested the user controller for basic register

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
 connectDB()
-  .then(()=>{
+  .then(() => {
     app.on("error", (error) => {
       console.log("ERROR:", error);
       throw error;
     });
-    app.listen(process.env.PORT||8000,()=>{
-      console.log("Server is running at port ",process.env.PORT)
-    })
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running at port ", process.env.PORT);
+    });
   })
   .catch((err) => {
     console.log("Mongo DB connection failded from index.js", err);
