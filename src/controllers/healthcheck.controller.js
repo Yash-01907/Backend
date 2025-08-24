4005d582097306a5c39248d2302273f3299188c3
Yash Pandey <yashpandey1907@gmail.com>
2025-08-24 13:53:20 +0530

    feat: implemented health check route

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,11 @@
-import {ApiError} from "../utils/ApiErrors.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import { ApiError } from "../utils/ApiErrors.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  //   return res.status(200).json(new ApiResponse(200, "OK"));
+  return res.status(200).json("OK");
+});
 
-export {
-    healthcheck
-    }
-    
+export { healthcheck };
